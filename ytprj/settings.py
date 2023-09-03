d4ace0c06a0bb9356148b300204ab30946ecb622
brijesh <brijeshkumarv598@gmail.com>
2023-09-04 00:25:41 +0530

    allowing all host

--- ytprj/settings.py
+++ ytprj/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-!^!2=*31z1jhv63r2u%lug++-^##i)hflpdmlwoc!2csj()9q(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
